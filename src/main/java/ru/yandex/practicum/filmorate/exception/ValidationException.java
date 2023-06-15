1ce191dbb4df0e0ea4061aaf00334a459f412fd8
Ustinov.vyacheslav <vustinov@fciit.ru>
2023-06-15 12:41:39 +0300

    Exception fix

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,12 +1,16 @@
 package ru.yandex.practicum.filmorate.exception;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Basic exception for invalid user or film data
  */
+@Slf4j
 public class ValidationException extends RuntimeException {
 
     public ValidationException(String message) {
         super(message);
+        log.error(message);
     }
 
 }
