c052c65e6247677981bd714fdd9bfb947f433a9a
Ustinov.vyacheslav <vustinov@fciit.ru>
2023-06-15 11:52:45 +0300

    Entity fix

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import ru.yandex.practicum.filmorate.validation.ReleaseDateConstraint;
 
 import javax.validation.constraints.NotBlank;
